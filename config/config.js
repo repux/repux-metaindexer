59d23853f7e5ccad15031da353396206ed912789
Przemysław Telążka <przemyslaw.telazka@workhq.com>
2018-07-17 12:36:17 +0200

    feat: don't generate config files for each environment, use one config with env variables instead

--- config/config.js
+++ config/config.js
@@ -1,14 +1,30 @@
-const fs = require('fs');
-
-let args = require('minimist')(process.argv.slice(2));
-let env = args.env || process.env.NODE_ENV || 'dev';
-
-let config = {};
-
-if (fs.existsSync(__dirname + '/config.' + env + '.js')) {
-    config = require('./config.' + env + '.js');
-}
-
-config.env = env;
+const config = {
+    startBlock: 0,
+    registryAddress: process.env.METAINDEXER_REGISTRY_ADDRESS || '0xc2d327375dd73b132d1171aadf7a205d3a9b7d8f',
+    tokenAddress: process.env.METAINDEXER_TOKEN_ADDRESS || '0xbd83c21e6f0a9547abe908c6faa02a55512d57b4',
+    tokenContractName: 'DemoToken',
+    ethereumHost: process.env.METAINDEXER_ETHEREUM_URL || 'http://repux-indexer-ganache-cli:8545',
+    lastBlockSaveInterval: 5000,
+    elasticsearch: {
+        host: process.env.METAINDEXER_ELASTICSEARCH_HOST_WITH_PORT || 'repux-indexer-es:9200',
+        protocol: 'http',
+        index: 'repux',
+        log: 'error',
+        proxy: {
+            host: process.env.METAINDEXER_ESPROXY_HOST || 'localhost',
+            port: process.env.METAINDEXER_ESPROXY_PORT || 9201,
+            ssl: {
+                enabled: process.env.METAINDEXER_ESPROXY_SSL || 0,
+                key: process.env.METAINDEXER_ESPROXY_SSL_KEY || null,
+                cert: process.env.METAINDEXER_ESPROXY_SSL_CERT || null
+            }
+        }
+    },
+    ipfs: {
+        httpUrl: process.env.METAINDEXER_IPFS_HTTP_URL || 'http://repux-indexer-ipfs:8080',
+        maxMetaFileSize: process.env.METAINDEXER_IPFS_MAX_META_FILE_SIZE || 512000
+    },
+    categorySeparator: '>'
+};
 
 module.exports = config;
