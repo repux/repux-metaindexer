f5f5ddfa5352bc3f83698283d22e6aec7b4a51c6
Przemysław Telążka <przemczan@gmail.com>
2018-09-20 13:03:20 +0200

    feat: ElasticSearch update command also reindexes all documents (#49)

--- src/elasticsearch/manager.ts
+++ src/elasticsearch/manager.ts
@@ -4,30 +4,60 @@ export class Manager {
 
     async reset(index: string, mappings: any, settings: any) {
         try {
-            await this.esClient.indices.delete(
-                {
-                    index, ignore: [404]
-                }
-            );
-            await this.esClient.indices.create(
-                {
-                    index,
-                    body: {settings, mappings}
-                }
-            );
+            await this.esClient.indices.delete({
+                index, ignore: [404]
+            });
+            await this.esClient.indices.create({
+                index,
+                body: {settings, mappings}
+            });
         } catch (e) {
             this.logger.error(e);
         }
     }
 
     async update(index: string, mappings: any, settings: any) {
+        const tempIndex = `${index}_reindex_temp`;
+
+        this.logger.info(`Updating index "${index}"`);
+
         try {
-            await this.esClient.indices.upgrade(
-                {
-                    index,
-                    body: {settings, mappings}
+            this.logger.info('deleting temporary index');
+            await this.esClient.indices.delete({index: tempIndex, ignore: [404]});
+
+            this.logger.info('updating current index mappings');
+            await this.esClient.indices.putMapping({
+                index,
+                type: index,
+                body: {properties: mappings[index].properties}
+            });
+
+            this.logger.info('creating temporary index');
+            await this.esClient.indices.create({
+                index: tempIndex,
+                body: {settings, mappings}
+            });
+
+            this.logger.info('copying documents to temporary index');
+            await this.esClient.reindex({
+                refresh: true,
+                body: {
+                    source: {index},
+                    dest: {index: tempIndex}
                 }
-            );
+            });
+
+            this.logger.info('copying documents back to current index');
+            await this.esClient.reindex({
+                refresh: true,
+                body: {
+                    source: {index: tempIndex},
+                    dest: {index}
+                }
+            });
+
+            this.logger.info('deleting temporary index');
+            await this.esClient.indices.delete({index: tempIndex, ignore: [404]});
         } catch (e) {
             this.logger.error(e);
         }
